8caa4836998f536fe6468df6663d79f5ab2ea42e
AriGL05 <cottoncandyplaysmc123@gmail.com>
2023-10-25 10:42:41 -0600

    Color to buttons and imp done

--- app/src/main/java/com/example/animplements/IntentsImplicitos.java
+++ app/src/main/java/com/example/animplements/IntentsImplicitos.java
@@ -2,7 +2,11 @@ package com.example.animplements;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IntentsImplicitos extends AppCompatActivity {
 
@@ -10,5 +14,39 @@ public class IntentsImplicitos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intents_implicitos);
+        Button epic = findViewById(R.id.epic);
+        Button hamilton = findViewById(R.id.hamilton);
+        Button heathers = findViewById(R.id.heathers);
+        Button cyclone = findViewById(R.id.cyclone);
+
+
+        epic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=mhPbrvRMj1o"));
+                startActivity(intent);
+            }
+        });
+        hamilton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=aPSWZUExZ8M&t=5645s&pp=ygUIaGFtaWx0b24%3D"));
+                startActivity(intent);
+            }
+        });
+        heathers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=cikbgjAJPBU&pp=ygUIaGVhdGhlcnM%3D"));
+                startActivity(intent);
+            }
+        });
+        cyclone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=Jf0xVYkyCT8&list=PLifAvT3M8XMqswlRaMLkvDyzjGd73kh3_"));
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/animplements/MainActivity.java
+++ app/src/main/java/com/example/animplements/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.animplements;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,15 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button imp = findViewById(R.id.implicitos);
+        Button exp = findViewById(R.id.explicitos);
         countdownText = findViewById(R.id.countdown_text);
 
         startCountdown();
+        imp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent IntImp = new Intent(MainActivity.this, IntentsImplicitos.class);
+                startActivity(IntImp);
+            }
+        });
+        exp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent IntExp = new Intent(MainActivity.this, IntentsExplicitos.class);
+                startActivity(IntExp);
+            }
+        });
     }
     private void startCountdown() {
-        new CountDownTimer(5000, 1000) { // Countdown for 5 seconds, with updates every 1 second
+        new CountDownTimer(5000, 1000) {
             public void onTick(long millisUntilFinished) {
-                // Update the TextView with the remaining seconds
+
                 int seconds = (int) (millisUntilFinished / 1000);
                 countdownText.setText(String.valueOf(seconds));
             }
