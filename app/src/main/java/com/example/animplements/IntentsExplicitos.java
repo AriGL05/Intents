b09eed9d3a874c340b54e5ac53e53d8705e2aed2
AriGL05 <cottoncandyplaysmc123@gmail.com>
2023-10-27 00:14:59 -0600

    More Animation stuff

--- app/src/main/java/com/example/animplements/IntentsExplicitos.java
+++ app/src/main/java/com/example/animplements/IntentsExplicitos.java
@@ -2,7 +2,11 @@ package com.example.animplements;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IntentsExplicitos extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class IntentsExplicitos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intents_explicitos);
+
+        Button anim = findViewById(R.id.animation);
+        Button about = findViewById(R.id.about);
+
+        anim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent anIntent = new Intent(IntentsExplicitos.this, Animation.class);
+                startActivity(anIntent);
+            }
+        });
     }
 }
